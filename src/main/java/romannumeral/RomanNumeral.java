a189c541cb1fdf5b08f00fb15243c756b2040b54
mgronis <m.gronis@gmail.com>
2015-10-04 09:02:39 +0200

    Roman Numeral: Added test and implementation for 1, 2, 3, 4

--- src/main/java/romannumeral/RomanNumeral.java
+++ src/main/java/romannumeral/RomanNumeral.java
@@ -2,8 +2,21 @@ package romannumeral;
 
 public class RomanNumeral {
 
-    public String convert(int i) {
-        return "I";
+    public static final String IV = "IV";
+    public static final String I = "I";
+
+    public String convert(int arabic) {
+
+        while (arabic > 0) {
+
+            if (arabic == 4) {
+                return IV;
+            } else {
+                return I + convert(--arabic);
+            }
+        }
+
+        return "";
     }
 
 }
--- src/test/java/romannumeral/RomanNumeralTest.java
+++ src/test/java/romannumeral/RomanNumeralTest.java
@@ -1,16 +1,31 @@
 package romannumeral;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class RomanNumeralTest {
 
     @Test
-    public void test(){
+    public void convert_1_shouldGive_I(){
         assertThat(new RomanNumeral().convert(1), is("I"));
     }
+
+    @Test
+    public void convert_2_shouldGive_II(){
+        assertThat(new RomanNumeral().convert(2), is("II"));
+    }
+
+    @Test
+    public void convert_3_shouldGive_III(){
+        assertThat(new RomanNumeral().convert(3), is("III"));
+    }
+
+    @Test
+    public void convert_4_shouldGive_IV(){
+        assertThat(new RomanNumeral().convert(4), is("IV"));
+    }
+
 
 }
