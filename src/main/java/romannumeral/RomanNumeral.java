99955d6ea4c2787d2d125840bfd3294c4180d427
mgronis <m.gronis@gmail.com>
2015-10-05 09:36:40 +0200

    Roman Numeral: Added tests for 50

--- src/main/java/romannumeral/RomanNumeral.java
+++ src/main/java/romannumeral/RomanNumeral.java
@@ -5,12 +5,21 @@ public class RomanNumeral {
     public static final String IV = "IV";
     public static final String I = "I";
     public static final String V = "V";
+    public static final String IX = "IX";
+    public static final String X = "X";
+    public static final String IL = "IL";
 
     public String convert(int arabicNumber) {
 
         while (arabicNumber > 0) {
-            if (arabicNumber == 9) {
-                return "IX";
+            if (arabicNumber >= 50) {
+                return "L" + convert(arabicNumber - 50);
+            }else if (arabicNumber == 49) {
+                return IL;
+            }else if (arabicNumber >= 10) {
+                return X + convert(arabicNumber - 10);
+            }else if (arabicNumber == 9) {
+                return IX;
             } else if (arabicNumber >= 5) {
                 return V + convert(arabicNumber - 5);
             } else if (arabicNumber == 4) {
