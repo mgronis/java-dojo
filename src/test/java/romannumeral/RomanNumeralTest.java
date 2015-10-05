3f556dcd04bb401ce12f4c20497937843f7750d4
mgronis <m.gronis@gmail.com>
2015-10-05 09:38:35 +0200

    Roman Numeral: Refactoring of test parameters

--- src/test/java/romannumeral/RomanNumeralTest.java
+++ src/test/java/romannumeral/RomanNumeralTest.java
@@ -19,7 +19,10 @@ public class RomanNumeralTest {
 
     @Parameters(name = "Arabic number {0} should be Roman number {1}")
     public static Collection<Object[]> data() {
-        return Lists.newArrayList(new Object[][]{{1, "I"}, {2, "II"}, {3, "III"}, {4, "IV"}, {5, "V"}, {6, "VI"}, {7, "VII"}, {8, "VIII"}, {9, "IX"}, {10, "X"}, {11, "XI"}, {48, "XXXXVIII"}, {49, "IL"}, {50, "L"}, {51, "LI"}});
+        return Lists.newArrayList(new Object[][]{
+                {1, "I"}, {2, "II"}, {3, "III"}, {4, "IV"}, {5, "V"}, {6, "VI"}, {7, "VII"}, {8, "VIII"},
+                {9, "IX"}, {10, "X"}, {11, "XI"}, {48, "XXXXVIII"}, {49, "IL"}, {50, "L"}, {51, "LI"}
+        });
     }
 
     public RomanNumeralTest(int arabicNumber, String romanNumber) {
