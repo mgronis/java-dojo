74bcc458bd645cc1174941a4e253f1fa55c9ca08
mgronis <m.gronis@gmail.com>
2015-10-05 16:02:27 +0200

    Roman Numeral: Refactored the ugly if else clause

--- src/main/java/romannumeral/RomanNumeral.java
+++ src/main/java/romannumeral/RomanNumeral.java
@@ -1,59 +1,44 @@
 package romannumeral;
 
 import com.google.common.base.Preconditions;
+import lombok.Value;
 
 public class RomanNumeral {
 
-    public static final String IV = "IV";
-    public static final String I = "I";
-    public static final String V = "V";
-    public static final String IX = "IX";
-    public static final String X = "X";
-    public static final String L = "L";
-    public static final String C = "C";
-    public static final String D = "D";
-    public static final String M = "M";
-    public static final String XL = "XL";
-    public static final String XC = "XC";
-    public static final String CD = "CD";
-    public static final String CM = "CM";
+    private static final RomanConstant[] ROMAN_CONSTANTS = new RomanConstant[]{
+            new RomanConstant(1000, "M"),
+            new RomanConstant(900, "CM"),
+            new RomanConstant(500, "D"),
+            new RomanConstant(400, "CD"),
+            new RomanConstant(100, "C"),
+            new RomanConstant(90, "XC"),
+            new RomanConstant(50, "L"),
+            new RomanConstant(40, "XL"),
+            new RomanConstant(10, "X"),
+            new RomanConstant(9, "IX"),
+            new RomanConstant(5, "V"),
+            new RomanConstant(4, "IV"),
+            new RomanConstant(1, "I")
+    };
 
     public String convert(int arabicNumber) {
 
         Preconditions.checkArgument(arabicNumber >= 0, "Can't handle negative numbers");
         Preconditions.checkArgument(arabicNumber < 5000, "Can't handle numbers greater than 4999");
 
-        while (arabicNumber > 0) {
-            if (arabicNumber >= 1000) {
-                return M + convert(arabicNumber - 1000);
-            }else if (arabicNumber >= 900) {
-                return CM + convert(arabicNumber - 900);
-            }else if (arabicNumber >= 500) {
-                return D + convert(arabicNumber - 500);
-            }else if (arabicNumber >= 400) {
-                return CD + convert(arabicNumber - 400);
-            }else if (arabicNumber >= 100) {
-                return C + convert(arabicNumber - 100);
-            }else if (arabicNumber >= 90) {
-                return XC + convert(arabicNumber - 90);
-            }else if (arabicNumber >= 50) {
-                return L + convert(arabicNumber - 50);
-            }else if (arabicNumber >= 40) {
-                return XL + convert(arabicNumber - 40);
-            }else if (arabicNumber >= 10) {
-                return X + convert(arabicNumber - 10);
-            }else if (arabicNumber >= 9) {
-                return IX + convert(arabicNumber - 9);
-            } else if (arabicNumber >= 5) {
-                return V + convert(arabicNumber - 5);
-            } else if (arabicNumber >= 4) {
-                return IV + convert(arabicNumber - 4);
-            } else {
-                return I + convert(arabicNumber - 1);
+        for (RomanConstant constant : ROMAN_CONSTANTS) {
+            if (arabicNumber >= constant.getArabic()) {
+                return constant.getRoman() + convert(arabicNumber - constant.getArabic());
             }
         }
 
         return "";
+    }
+
+    @Value
+    private static class RomanConstant {
+        private final int arabic;
+        private final String roman;
     }
 
 }
