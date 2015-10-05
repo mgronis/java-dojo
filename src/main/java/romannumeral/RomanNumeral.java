2e477f3d759e53023e3c57af9dab2c2dffbe8b6b
mgronis <m.gronis@gmail.com>
2015-10-05 13:50:18 +0200

    Roman Numeral: Added more test to handle 500

--- src/main/java/romannumeral/RomanNumeral.java
+++ src/main/java/romannumeral/RomanNumeral.java
@@ -8,12 +8,25 @@ public class RomanNumeral {
     public static final String IX = "IX";
     public static final String X = "X";
     public static final String IL = "IL";
+    public static final String L = "L";
+    public static final String IC = "IC";
+    public static final String C = "C";
+    public static final String ID = "ID";
+    public static final String D = "D";
 
     public String convert(int arabicNumber) {
 
         while (arabicNumber > 0) {
-            if (arabicNumber >= 50) {
-                return "L" + convert(arabicNumber - 50);
+            if (arabicNumber >= 500) {
+                return D + convert(arabicNumber - 500);
+            }else if (arabicNumber == 499) {
+                return ID;
+            }else if (arabicNumber >= 100) {
+                return C + convert(arabicNumber - 100);
+            }else if (arabicNumber == 99) {
+                return IC;
+            }else if (arabicNumber >= 50) {
+                return L + convert(arabicNumber - 50);
             }else if (arabicNumber == 49) {
                 return IL;
             }else if (arabicNumber >= 10) {
